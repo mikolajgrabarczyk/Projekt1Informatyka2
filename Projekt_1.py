ea60dda4217f40b44207b1431827ca68f56d78f8
mikolajgrabarczyk <mikolaj_grabarczyk@o2.pl>
2023-04-26 17:00:29 +0200

    utworzenie algorytmu Hirvonena

--- Projekt_1.py
+++ Projekt_1.py
@@ -24,3 +24,70 @@ class Transformacje:
         self.flat = (self.a - self.b) / self.a
         self.e = sqrt(2 * self.flat - self.flat ** 2) # eccentricity  WGS84:0.0818191910428 
         self.e2 = (2 * self.flat - self.flat ** 2) # eccentricity**2
+        
+        def xyz2plh(self, X, Y, Z, output = 'dec_degree'):
+        """
+        Algorytm Hirvonena - algorytm transformacji współrzędnych ortokartezjańskich (x, y, z)
+        na współrzędne geodezyjne długość szerokość i wysokośc elipsoidalna (phi, lam, h). Jest to proces iteracyjny. 
+        W wyniku 3-4-krotneej iteracji wyznaczenia wsp. phi można przeliczyć współrzędne z dokładnoscią ok 1 cm.     
+        Parameters
+        ----------
+        X, Y, Z : FLOAT
+             współrzędne w układzie orto-kartezjańskim, 
+
+        Returns
+        -------
+        lat
+            [stopnie dziesiętne] - szerokość geodezyjna
+        lon
+            [stopnie dziesiętne] - długośc geodezyjna.
+        h : TYPE
+            [metry] - wysokość elipsoidalna
+        output [STR] - optional, defoulf 
+            dec_degree - decimal degree
+            dms - degree, minutes, sec
+        """
+        r   = sqrt(X**2 + Y**2)           # promień
+        lat_prev = atan(Z / (r * (1 - self.e2)))    # pierwsze przybliilizenie
+        lat = 0
+        while abs(lat_prev - lat) > 0.000001/206265:    
+            lat_prev = lat
+            N = self.a / sqrt(1 - self.e2 * sin(lat_prev)**2)
+            h = r / cos(lat_prev) - N
+            lat = atan((Z/r) * (((1 - self.e2 * N/(N + h))**(-1))))
+        lon = atan(Y/X)
+        N = self.a / sqrt(1 - self.e2 * (sin(lat))**2);
+        h = r / cos(lat) - N       
+        if output == "dec_degree":
+            return degrees(lat), degrees(lon), h 
+        elif output == "dms":
+            lat = self.deg2dms(degrees(lat))
+            lon = self.deg2dms(degrees(lon))
+            return f"{lat[0]:02d}:{lat[1]:02d}:{lat[2]:.2f}", f"{lon[0]:02d}:{lon[1]:02d}:{lon[2]:.2f}", f"{h:.3f}"
+        else:
+            raise NotImplementedError(f"{output} - output format not defined")
+            
+    
+            
+    
+            
+
+
+    def s_A_z2neu (self , s, A, Zx):
+        A = np.deg2rad(A)
+        Zx = np.deg2rad(Zx)
+        n = s * np.sin(Zx) * np.cos(A)
+        e = s * np.sin(Zx) * np.sin(A)
+        u = s * np.cos(Zx)
+        
+        return n, e, u
+
+
+
+    def neu2dxyz(self , F1, L1, n, e, u):
+        
+        R = np.array([[-np.sin(F1) * np.cos(L1), -np.sin(L1), np.cos(F1) * np.cos(L1)],
+                     [-np.sin(F1) * np.sin(L1), np.cos(L1), np.cos(F1) * np.sin(L1)],
+                     [np.cos(F1), 0, np.sin(F1)]])
+        dx = np.linalg.inv(R.transpose()) @ np.array([n, e, u])
+        return dx
