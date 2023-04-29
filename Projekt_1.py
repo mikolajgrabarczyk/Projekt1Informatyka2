7a18c987d00c1804aeb6472a639c811b33f37999
mikolajgrabarczyk <101605618+mikolajgrabarczyk@users.noreply.github.com>
2023-04-29 23:14:02 +0200

    Aktualizacja algorytmu Hirvonena
    Oraz dodanie przekształcenia współrzędnych geodezyjnych na geocentryczne

--- Projekt_1.py
+++ Projekt_1.py
@@ -1,93 +1,51 @@
 class Transformacje:
-    def __init__(self, model: str = "wgs84"):
-        """
-        Parametry elipsoid:
-            a - duża półoś elipsoidy - promień równikowy
-            b - mała półoś elipsoidy - promień południkowy
-            flat - spłaszczenie
-            ecc2 - mimośród^2
-        + WGS84: https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
-        + Inne powierzchnie odniesienia: https://en.wikibooks.org/wiki/PROJ.4#Spheroid
-        + Parametry planet: https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html
-        """
-        if model == "wgs84":
-            self.a = 6378137.0 # semimajor_axis
-            self.b = 6356752.31424518 # semiminor_axis
-        elif model == "grs80":
+    def __init__(self, ellipsoid="wgs84"):
+        if ellipsoid == "wgs84":
+            self.a = 6378137.0
+            self.b = 6356752.31424518
+        elif ellipsoid == "grs80":
             self.a = 6378137.0
             self.b = 6356752.31414036
-        elif model == "mars":
-            self.a = 3396900.0
-            self.b = 3376097.80585952
         else:
-            raise NotImplementedError(f"{model} model not implemented")
-        self.flat = (self.a - self.b) / self.a
-        self.e = sqrt(2 * self.flat - self.flat ** 2) # eccentricity  WGS84:0.0818191910428 
-        self.e2 = (2 * self.flat - self.flat ** 2) # eccentricity**2
-        
-        def xyz2plh(self, X, Y, Z, output = 'dec_degree'):
-        """
-        Algorytm Hirvonena - algorytm transformacji współrzędnych ortokartezjańskich (x, y, z)
-        na współrzędne geodezyjne długość szerokość i wysokośc elipsoidalna (phi, lam, h). Jest to proces iteracyjny. 
-        W wyniku 3-4-krotneej iteracji wyznaczenia wsp. phi można przeliczyć współrzędne z dokładnoscią ok 1 cm.     
-        Parameters
-        ----------
-        X, Y, Z : FLOAT
-             współrzędne w układzie orto-kartezjańskim, 
+            print("This ellipsoid is not supported")
+        self.flattening = (self.a - self.b) / self.a
+        self.e2 = sqrt(2 * self.flattening - self.flattening ** 2)
 
-        Returns
-        -------
-        lat
-            [stopnie dziesiętne] - szerokość geodezyjna
-        lon
-            [stopnie dziesiętne] - długośc geodezyjna.
-        h : TYPE
-            [metry] - wysokość elipsoidalna
-        output [STR] - optional, defoulf 
-            dec_degree - decimal degree
-            dms - degree, minutes, sec
-        """
-        r   = sqrt(X**2 + Y**2)           # promień
-        lat_prev = atan(Z / (r * (1 - self.e2)))    # pierwsze przybliilizenie
-        lat = 0
-        while abs(lat_prev - lat) > 0.000001/206265:    
-            lat_prev = lat
-            N = self.a / sqrt(1 - self.e2 * sin(lat_prev)**2)
-            h = r / cos(lat_prev) - N
-            lat = atan((Z/r) * (((1 - self.e2 * N/(N + h))**(-1))))
-        lon = atan(Y/X)
-        N = self.a / sqrt(1 - self.e2 * (sin(lat))**2);
-        h = r / cos(lat) - N       
-        if output == "dec_degree":
-            return degrees(lat), degrees(lon), h 
-        elif output == "dms":
-            lat = self.deg2dms(degrees(lat))
-            lon = self.deg2dms(degrees(lon))
-            return f"{lat[0]:02d}:{lat[1]:02d}:{lat[2]:.2f}", f"{lon[0]:02d}:{lon[1]:02d}:{lon[2]:.2f}", f"{h:.3f}"
-        else:
-            raise NotImplementedError(f"{output} - output format not defined")
-            
-    
-            
-    
-            
+    def hirvonen(self, X, Y, Z):
+        r = math.sqrt(X ** 2 + Y ** 2)
+        fi_n = math.atan(Z / (r * (1 - self.e2)))
+        eps = 0.000001 / 3600 * math.pi / 180
+        fi = fi_n * 2
+        while np.abs(fi_n - fi) > eps:
+            fi = fi_n
+            N = self.a / np.sqrt(1 - self.e2 * np.sin(fi_n) ** 2)
+            h = r / np.cos(fi_n) - N
+            fi_n = math.atan(Z / (r * (1 - self.e2 * (N / (N + h)))))
+        lam = math.atan(Y / X)
+        h = r / np.cos(fi_n) - N
+        return fi, lam, h
+    def geodetic_to_geocentric(self, fi, lam, h):
+        N = self.a / math.sqrt(1 - self.e2 * math.sin(fi) ** 2)
+        X = (N + h) * math.cos(fi) * math.cos(lam)
+        Y = (N + h) * math.cos(fi) * math.sin(lam)
+        Z = (N * (1 - self.e2) + h) * math.sin(fi)
+        return (X, Y, Z)
 
+    def compute_neu(self, X0, Y0, Z0, X, Y, Z):
+        fi, lam, h = self.hirvonen(X, Y, Z)
 
-    def s_A_z2neu (self , s, A, Zx):
-        A = np.deg2rad(A)
-        Zx = np.deg2rad(Zx)
-        n = s * np.sin(Zx) * np.cos(A)
-        e = s * np.sin(Zx) * np.sin(A)
-        u = s * np.cos(Zx)
-        
-        return n, e, u
+        delta_X = X - X0
+        delta_Y = Y - Y0
+        delta_Z = Z - Z0
 
+        R = np.matrix([((-np.sin(fi) * np.cos(lam)), (-np.sin(fi) * np.sin(lam)), (np.cos(fi))),
+                       ((-np.sin(lam)), (np.cos(lam)), (0)),
+                       ((np.cos(fi) * np.cos(lam)), (np.cos(fi) * np.sin(lam)), (np.sin(fi)))])
 
-
-    def neu2dxyz(self , F1, L1, n, e, u):
-        
-        R = np.array([[-np.sin(F1) * np.cos(L1), -np.sin(L1), np.cos(F1) * np.cos(L1)],
-                     [-np.sin(F1) * np.sin(L1), np.cos(L1), np.cos(F1) * np.sin(L1)],
-                     [np.cos(F1), 0, np.sin(F1)]])
-        dx = np.linalg.inv(R.transpose()) @ np.array([n, e, u])
-        return dx
+        d = np.matrix([delta_X, delta_Y, delta_Z])
+        d = d.T
+        neu = R * d
+        n = neu[0, 0]
+        e = neu[1, 0]
+        u = neu[2, 0]
+        return (n, e, u)
