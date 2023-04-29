b3ee006494f67a252902c3633bbf3cd25c6b14e0
mikolajgrabarczyk <101605618+mikolajgrabarczyk@users.noreply.github.com>
2023-04-29 23:16:01 +0200

    Dodanie przekształcenia Fi, Lam do układu 00 i 92

--- Projekt_1.py
+++ Projekt_1.py
@@ -49,3 +49,76 @@ class Transformacje:
         e = neu[1, 0]
         u = neu[2, 0]
         return (n, e, u)
+    def geo_to_flat_2000(self, fi, lam):
+        m = 0.999923
+        N = self.a / math.sqrt(1 - self.e2 * math.sin(fi) ** 2)
+        t = np.tan(fi)
+        e_2 = self.e2 / (1 - self.e2)
+        n2 = e_2 * (np.cos(fi)) ** 2
+
+        lam = math.degrees(lam)
+        if lam > 13.5 and lam < 16.5:
+            s = 5
+            lam0 = 15
+        elif lam > 16.5 and lam < 19.5:
+            s = 6
+            lam0 = 18
+        elif lam > 19.5 and lam < 22.5:
+            s = 7
+            lam0 = 21
+        elif lam > 22.5 and lam < 25.5:
+            s = 8
+            lam0 = 24
+
+        lam = math.radians(lam)
+        lam0 = math.radians(lam0)
+        l = lam - lam0
+
+        A0 = 1 - (self.e2 / 4) - ((3 * (self.e2 ** 2)) / 64) - ((5 * (self.e2 ** 3)) / 256)
+        A2 = (3 / 8) * (self.e2 + ((self.e2 ** 2) / 4) + ((15 * (self.e2 ** 3)) / 128))
+        A4 = (15 / 256) * (self.e2 ** 2 + ((3 * (self.e2 ** 3)) / 4))
+        A6 = (35 * (self.e2 ** 3)) / 3072
+
+        sig = self.a * ((A0 * fi) - (A2 * np.sin(2 * fi)) + (A4 * np.sin(4 * fi)) - (A6 * np.sin(6 * fi)))
+
+        x = sig + ((l ** 2) / 2) * N * np.sin(fi) * np.cos(fi) * (
+                    1 + ((l ** 2) / 12) * ((math.cos(fi)) ** 2) * (5 - t ** 2 + 9 * n2 + 4 * (n2 ** 2)) + (
+                        (l ** 4) / 360) * ((math.cos(fi)) ** 4) * (
+                                61 - (58 * (t ** 2)) + (t ** 4) + (270 * n2) - (330 * n2 * (t ** 2))))
+        y = l * (N * math.cos(fi)) * (1 + ((((l ** 2) / 6) * (math.cos(fi)) ** 2) * (1 - t ** 2 + n2)) + (
+                    ((l ** 4) / (120)) * (math.cos(fi) ** 4)) * (
+                                                  5 - (18 * (t ** 2)) + (t ** 4) + (14 * n2) - (58 * n2 * (t ** 2))))
+
+        x00 = round(m * x, 3)
+        y00 = round(m * y + (s * 1000000) + 500000, 3)
+
+        return (x00, y00)
+
+    def geo_to_flat_1992(self, fi, lam):
+        m_0 = 0.9993
+        N = self.a / (np.sqrt(1 - self.e2 * np.sin(fi) ** 2))
+        t = np.tan(fi)
+        e_2 = self.e2 / (1 - self.e2)
+        n2 = e_2 * (np.cos(fi)) ** 2
+
+        lam_0 = math.radians(19)
+        l = lam - lam_0
+
+        A0 = 1 - (self.e2 / 4) - ((3 * (self.e2 ** 2)) / 64) - ((5 * (self.e2 ** 3)) / 256)
+        A2 = (3 / 8) * (self.e2 + ((self.e2 ** 2) / 4) + ((15 * (self.e2 ** 3)) / 128))
+        A4 = (15 / 256) * (self.e2 ** 2 + ((3 * (self.e2 ** 3)) / 4))
+        A6 = (35 * (self.e2 ** 3)) / 3072
+
+        sig = self.a * ((A0 * fi) - (A2 * np.sin(2 * fi)) + (A4 * np.sin(4 * fi)) - (A6 * np.sin(6 * fi)))
+
+        x = sig + ((l ** 2) / 2) * N * np.sin(fi) * np.cos(fi) * (
+                    1 + ((l ** 2) / 12) * ((math.cos(fi)) ** 2) * (5 - t ** 2 + 9 * n2 + 4 * (n2 ** 2)) + (
+                        (l ** 4) / 360) * ((math.cos(fi)) ** 4) * (
+                                61 - (58 * (t ** 2)) + (t ** 4) + (270 * n2) - (330 * n2 * (t ** 2))))
+        y = l * (N * math.cos(fi)) * (1 + ((((l ** 2) / 6) * (math.cos(fi)) ** 2) * (1 - t ** 2 + n2)) + (
+                    ((l ** 4) / (120)) * (math.cos(fi) ** 4)) * (
+                                                  5 - (18 * (t ** 2)) + (t ** 4) + (14 * n2) - (58 * n2 * (t ** 2))))
+
+        x92 = round(m_0 * x - 5300000, 3)
+        y92 = round(m_0 * y + 500000, 3)
+        return x92, y92
